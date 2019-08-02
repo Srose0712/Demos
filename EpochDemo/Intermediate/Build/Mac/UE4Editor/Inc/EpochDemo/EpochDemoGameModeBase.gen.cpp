48c3d9901fb561085bd7b545231391868342ace6
Samuel Blake Rose <sambrose0712@gmail.com>
2019-08-02 15:11:21 -0500

    Initial Upload

---
+++ EpochDemo/Intermediate/Build/Mac/UE4Editor/Inc/EpochDemo/EpochDemoGameModeBase.gen.cpp
@@ -0,0 +1,86 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/GeneratedCppIncludes.h"
+#include "EpochDemo/EpochDemoGameModeBase.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeEpochDemoGameModeBase() {}
+// Cross Module References
+	EPOCHDEMO_API UClass* Z_Construct_UClass_AEpochDemoGameModeBase_NoRegister();
+	EPOCHDEMO_API UClass* Z_Construct_UClass_AEpochDemoGameModeBase();
+	ENGINE_API UClass* Z_Construct_UClass_AGameModeBase();
+	UPackage* Z_Construct_UPackage__Script_EpochDemo();
+// End Cross Module References
+	void AEpochDemoGameModeBase::StaticRegisterNativesAEpochDemoGameModeBase()
+	{
+	}
+	UClass* Z_Construct_UClass_AEpochDemoGameModeBase_NoRegister()
+	{
+		return AEpochDemoGameModeBase::StaticClass();
+	}
+	struct Z_Construct_UClass_AEpochDemoGameModeBase_Statics
+	{
+		static UObject* (*const DependentSingletons[])();
+#if WITH_METADATA
+		static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[];
+#endif
+		static const FCppClassTypeInfoStatic StaticCppClassTypeInfo;
+		static const UE4CodeGen_Private::FClassParams ClassParams;
+	};
+	UObject* (*const Z_Construct_UClass_AEpochDemoGameModeBase_Statics::DependentSingletons[])() = {
+		(UObject* (*)())Z_Construct_UClass_AGameModeBase,
+		(UObject* (*)())Z_Construct_UPackage__Script_EpochDemo,
+	};
+#if WITH_METADATA
+	const UE4CodeGen_Private::FMetaDataPairParam Z_Construct_UClass_AEpochDemoGameModeBase_Statics::Class_MetaDataParams[] = {
+		{ "HideCategories", "Info Rendering MovementReplication Replication Actor Input Movement Collision Rendering Utilities|Transformation" },
+		{ "IncludePath", "EpochDemoGameModeBase.h" },
+		{ "ModuleRelativePath", "EpochDemoGameModeBase.h" },
+		{ "ShowCategories", "Input|MouseInput Input|TouchInput" },
+	};
+#endif
+	const FCppClassTypeInfoStatic Z_Construct_UClass_AEpochDemoGameModeBase_Statics::StaticCppClassTypeInfo = {
+		TCppClassTypeTraits<AEpochDemoGameModeBase>::IsAbstract,
+	};
+	const UE4CodeGen_Private::FClassParams Z_Construct_UClass_AEpochDemoGameModeBase_Statics::ClassParams = {
+		&AEpochDemoGameModeBase::StaticClass,
+		nullptr,
+		&StaticCppClassTypeInfo,
+		DependentSingletons,
+		nullptr,
+		nullptr,
+		nullptr,
+		ARRAY_COUNT(DependentSingletons),
+		0,
+		0,
+		0,
+		0x009002A8u,
+		METADATA_PARAMS(Z_Construct_UClass_AEpochDemoGameModeBase_Statics::Class_MetaDataParams, ARRAY_COUNT(Z_Construct_UClass_AEpochDemoGameModeBase_Statics::Class_MetaDataParams))
+	};
+	UClass* Z_Construct_UClass_AEpochDemoGameModeBase()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			UE4CodeGen_Private::ConstructUClass(OuterClass, Z_Construct_UClass_AEpochDemoGameModeBase_Statics::ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AEpochDemoGameModeBase, 2313908658);
+	template<> EPOCHDEMO_API UClass* StaticClass<AEpochDemoGameModeBase>()
+	{
+		return AEpochDemoGameModeBase::StaticClass();
+	}
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AEpochDemoGameModeBase(Z_Construct_UClass_AEpochDemoGameModeBase, &AEpochDemoGameModeBase::StaticClass, TEXT("/Script/EpochDemo"), TEXT("AEpochDemoGameModeBase"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AEpochDemoGameModeBase);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ EpochDemo/Intermediate/Build/Mac/UE4Editor/Inc/EpochDemo/EpochDemoGameModeBase.generated.h
@@ -0,0 +1,92 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef EPOCHDEMO_EpochDemoGameModeBase_generated_h
+#error "EpochDemoGameModeBase.generated.h already included, missing '#pragma once' in EpochDemoGameModeBase.h"
+#endif
+#define EPOCHDEMO_EpochDemoGameModeBase_generated_h
+
+#define EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_RPC_WRAPPERS
+#define EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAEpochDemoGameModeBase(); \
+	friend struct Z_Construct_UClass_AEpochDemoGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AEpochDemoGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/EpochDemo"), NO_API) \
+	DECLARE_SERIALIZER(AEpochDemoGameModeBase)
+
+
+#define EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAEpochDemoGameModeBase(); \
+	friend struct Z_Construct_UClass_AEpochDemoGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AEpochDemoGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/EpochDemo"), NO_API) \
+	DECLARE_SERIALIZER(AEpochDemoGameModeBase)
+
+
+#define EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AEpochDemoGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEpochDemoGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEpochDemoGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEpochDemoGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEpochDemoGameModeBase(AEpochDemoGameModeBase&&); \
+	NO_API AEpochDemoGameModeBase(const AEpochDemoGameModeBase&); \
+public:
+
+
+#define EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AEpochDemoGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AEpochDemoGameModeBase(AEpochDemoGameModeBase&&); \
+	NO_API AEpochDemoGameModeBase(const AEpochDemoGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AEpochDemoGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AEpochDemoGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AEpochDemoGameModeBase)
+
+
+#define EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_12_PROLOG
+#define EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_RPC_WRAPPERS \
+	EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_INCLASS \
+	EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+template<> EPOCHDEMO_API UClass* StaticClass<class AEpochDemoGameModeBase>();
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID EpochDemo_Source_EpochDemo_EpochDemoGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
